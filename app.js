ce286e87cbf147394a0ee6f2252b1c0973d58070
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2025-11-09 09:22:41 -0600

    add mongoDB connection and create Mongoose schemas for users.js and clothingItems.js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Server is running successfully");
