5a04563a66c39078ca8e76f0086f6a6c73174bb5
Alex Xavier Ruiz <ruiz.xavier@ymail.com>
2026-01-10 16:30:51 -0600

    corrected error handling paths

--- utils/errors.js
+++ utils/errors.js
@@ -5,11 +5,11 @@ const NOT_FOUND = 404;
 const CONFLICT = 409;
 const DEFAULT_ERROR = 500;
 
-const BadRequestError = require("./BadRequestError");
-const UnauthorizedError = require("./UnauthorizedError");
-const ForbiddenError = require("./ForbiddenError");
-const NotFoundError = require("./NotFoundError");
-const ConflictError = require("./ConflictError");
+const BadRequestError = require("./errors/BadRequestError");
+const UnauthorizedError = require("./errors/UnauthorizedError");
+const ForbiddenError = require("./errors/ForbiddenError");
+const NotFoundError = require("./errors/NotFoundError");
+const ConflictError = require("./errors/ConflictError");
 
 module.exports = {
   BAD_REQUEST,
